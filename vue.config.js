38ed6c6f1b041705bc7d2a389b8ac3a17f6a5309
qiuqiu <123>
2019-07-10 09:40:10 +0800

    7109.40

--- vue.config.js
+++ vue.config.js
@@ -1,34 +1,37 @@
+
+
 const path = require('path');
 module.exports = {
     devServer: {
-        open: true,
         proxy: {
             "/a": {
                 target: "https://www.jianshu.com",
                 changOrigin: true,
                 pathRewrite: {
-                    '^/a': '/'
-                }
-
-            }
-        },
-        configureWebpack: {
-            resolve: {
-                alias: {
-                    "@": path.resolve(__dirname, "./src/"),
-                    "api": path.resolve(__dirname, "./src/api/"),
-                    "assets": path.resolve(__dirname, "./src/assets/"),
-                    "common": path.resolve(__dirname, "./src/common/"),
-                    "components": path.resolve(__dirname, "./src/components/"),
-                    "router": path.resolve(__dirname, "./src/router/"),
-                    "store": path.resolve(__dirname, "./src/store/"),
-                    "utils": path.resolve(__dirname, "./src/utils/"),
-                    "views": path.join(__dirname, "./src/views/"),
+                    pathRewrite: {
+                        '^/a': '/'
+                    }
 
                 }
+            },
+            configureWebpack: {
+                resolve: {
+                    alias: {
+                        "@": path.resolve(__dirname, "./src/"),
+                        "api": path.resolve(__dirname, "./src/api/"),
+                        "assets": path.resolve(__dirname, "./src/assets/"),
+                        "common": path.resolve(__dirname, "./src/common/"),
+                        "components": path.resolve(__dirname, "./src/components/"),
+                        "router": path.resolve(__dirname, "./src/router/"),
+                        "store": path.resolve(__dirname, "./src/store/"),
+                        "utils": path.resolve(__dirname, "./src/utils/"),
+                        "views": path.join(__dirname, "./src/views/"),
 
+                    }
+
+                }
             }
+
         }
-
     }
-    }
+}
