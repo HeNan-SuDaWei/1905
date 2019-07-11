3564e369c28857764e60c95b112c7325746d2992
su <118719559@qq.com>
2019-07-11 20:59:10 +0800

    111

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -8,35 +8,21 @@
                 </span>
             </div>
             <div class="recommend-collection">
-<<<<<<< HEAD
                 <router-link v-for="(item,index) in nav[val]" :to="item.path"  tag="a" :key="index">{{item.title}}</router-link>
             </div>
             <hr>
             <ul class="flow-list-container">
                 <router-link class="not-flow" v-for="(item,index) in content" :key="index"  :to="'/listContent/'+item.object.data.slug" tag='li' >
-=======
-                <router-link v-for="(item,index) in nav[val]" :to="item.path" @click="handleClick(index)" tag="a" :key="index">{{item.title}}</router-link>
-            </div>
-            <hr>
-            <ul class="flow-list-container">
-                <li class="not-flow" v-for="(item,index) in content"
-                :key="index">
->>>>>>> 1ddfce6278a4ed9b89c74083324390b690ba0f26
                     <h3 class="title">{{item.object.data.title}}</h3>
                     <span class="warp-img">
                         <img :src="item.object.data.list_image_url" alt="">
                     </span>
-<<<<<<< HEAD
                     <p class="abstract">    
-=======
-                    <p class="abstract">
->>>>>>> 1ddfce6278a4ed9b89c74083324390b690ba0f26
                         {{item.object.data.public_abbr}}
                     </p>
                     <i class="icon iconfont"><em class="icont">&#xe675;</em><span class="sp">{{item.object.data.total_fp_amount}}{{item.object.data.user.nickname}}</span></i>
                     <i class="icon iconfont">&#xe606;<span class="sp">{{item.object.data.public_comments_count}}</span></i>
                     <i class="icon iconfont">&#xe6db;<span class="sp">{{item.object.data.likes_count}}</span></i>
-<<<<<<< HEAD
                 </router-link>
 
                 <div class="copy">
@@ -50,13 +36,6 @@
 
             </ul>
             <router-view></router-view>
-=======
-                </li>
-
-            <div class="copy">
-                <span>展开更多文章</span><i class="icon iconfont">&#xe658;</i>
-            </div>
->>>>>>> 1ddfce6278a4ed9b89c74083324390b690ba0f26
 
             <hr>
             <div class="download-guide">
@@ -87,14 +66,10 @@
             })
 
             this.content = data
-<<<<<<< HEAD
             console.log(data)
             
             
 
-=======
-            
->>>>>>> 1ddfce6278a4ed9b89c74083324390b690ba0f26
 
 
         },
@@ -177,7 +152,6 @@
     }
 </script>
 <style>
-<<<<<<< HEAD
   
 
     .data-head {
@@ -372,197 +346,4 @@
         color: red;
         font-style: normal;
     }
-=======
-    *{
-    margin: 0;
-    padding: 0;
-}
-.data-head{
-    width: 100%;
-}
-
-.jianshu-head{
-    font-size: .32rem;
-    width: 100%;
-    height: 1rem;
-    display: flex;
-    align-items: center;
-    padding: .5rem .32rem;
-    border-bottom: 1px solid #757575;
-    position: fixed;
-    
-}
-
-.recommend{
-    position: fixed;top:1rem;
-    overflow-y:scroll;
-    height: 12.34rem;
-   
-}
-
-.head-img{
-    width: 1.12rem;
-    height: .94rem;
-}
-
-.slogan{
-    margin-left: .24rem;
-    color: #333;
-}
-
-.download{
-    position: absolute;right: 0;
-    color: #ea6f5a;
-    padding: .24rem .7rem
-}
-
-.head-left{
-    position: absolute;right: 0;
-    padding-right: .3rem
-
-}
-
-.top-title{
-    display: flex;
-    height: .84rem;
-    align-items: center;
-}
-
-.special{
-    font-size: .28rem;
-    margin-left: .4rem
-}
-
-
-.collection-change{
-    position: absolute;right: .8rem;
-}
-
-.collection-change .iconfont{
-    font-size: .24rem;
-    position: absolute;right: .8rem;
-}
-
-.recommend-collection{
-    width: 100%;
-    height: 1.6rem;
-    display: flex;
-    flex-wrap: wrap;
-    padding-left: .4rem;
-    align-items: center;
-    
-   
-    
-}
-
-.recommend-collection a{
-    display: block;
-    margin-right: .2rem;
-    font-size: .32rem;
-    border: 1px solid #ea6f5a;
-    border-radius: .1rem;
-    padding: .1rem;
-    color: #ea6f5a;
-}
-
-hr{
-    height: .2rem;
-    background-color: #f5f5f5;
-    border: none;
-}
-
-.flow-list-container{
-    width: 100%;
-    height: 100%;
-    margin-top: .1rem;
-   
-    
-}
-
-.title{
-    float: left;
-    font-size: .34rem;
-    width: 70%;
-    line-height: .5rem;
-    margin-bottom: .1rem
-}
-
-.not-flow{
-    width: 100%;
-    line-height: .35rem;
-    margin-bottom: .1rem;
-    border-bottom: 1px solid #757575;
-     padding: .36rem; 
-
-    
-}
-
-.not-flow .iconfont{
-    font-size: .32rem;
-}
-
-
-
-.warp-img{
-    float: right;
-    margin-top: .2rem;
-}
-
-.warp-img img{
-     width: 2rem;
-    height: 2rem;
-    margin-top: .1rem
-}
-
-.abstract{
-    width: 70%;
-    font-size: .26rem;
-    color: #999;
-    margin-bottom: .3rem;
-}
-
-.copy{
-    width: 100%;
-    height: 1.1rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    color: #969696;
-    font-size: .32rem;
-}
-
-.copy i{
-    font-size: .32rem;
-}
-
-.download-guide{
-    width: 100%;
-    height: 4.4rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    
-}
-
-.download-guide a{
-    display: block;
-    width: 4.8rem;
-    height: .9rem;
-    color: #f5f5f5;
-    background-color: #ea6f5a;
-    text-align: center;
-    font-size: .32rem; 
-    line-height: .9rem;
-   
-}
-
-.iconfont .sp{
-    font-size: .24rem;
-}
-
-.icont{
-    color: red;
-    font-style:normal;
-}
->>>>>>> 1ddfce6278a4ed9b89c74083324390b690ba0f26
 </style>
